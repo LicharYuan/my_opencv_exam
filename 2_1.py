682f059b6c16ab08f3562bcb7e3e9b7dd2f0869d
LicharYuan <ylc0003@qq.com>
2017-12-02 20:33:00 +0800

    first commit

---
+++ 2_1.py
@@ -0,0 +1,18 @@
+# import numpy as np
+# import cv2
+# img=np.zeros((3,3),dtype=np.uint8)
+# img=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+# #cv2.cvtColor turn the gary to BGR
+# print(img)
+#
+# #-----------------------------------------------
+
+# import cv2
+# grayImage=cv2.imread('/home/lichar/database/test/000.jpg',cv2.IMREAD_GRAYSCALE)
+# cv2.imwrite('/home/lichar/database/test/000_gary.jpg',grayImage)
+# #read the pic as gray model
+# #imwrite() ask the the pics as BGR or Gray mode and every channel need certain bits (bmp need 8 bits PNG 8 or 16)
+# #----------------------------------------------------
+
+
+
